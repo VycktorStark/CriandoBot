4447156cf475df7f4c1070d859b49f26c6b49880
Vycktor Stark <huho387@gmail.com>
2020-08-10 17:21:28 -0400

    adicionando sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,4 +2,11 @@ module.exports = {
   title: 'Criando bot',
   description: 'Criador do Projeto: VycktorStark',
   base: '/CriandoBot/'
+  themeConfig:{
+  	sidebar: [
+  	['/', 'Inicio'],
+  	['/CriandoBot/aulas/Piloto.html', 'Primeira Aula'],
+  	['/CriandoBot/aulas/Comunicacao.html', 'Segunda Aula']
+  	]
+  }
 }
