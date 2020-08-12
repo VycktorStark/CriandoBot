eb57046b4cd2ec5a408769bad1c7be1454291f6d
Vycktor Stark <huho387@gmail.com>
2020-08-12 09:38:04 -0400

    Corrigindo alguns caminhos

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,31 @@ module.exports = {
   base: '/CriandoBot/',
   themeConfig:{
   	sidebar: [
-  	['/', 'Inicio']
+  	['/', 'Inicio'],
+  	['criandobot/aulas/Piloto.html', 'Primeira Aula'],
+  	['criandobot/aulas/Comunicacao.html', 'Segunda Aula'],
   	]
-  }
+  },
+  head: [
+    ['link', { rel: 'icon', href: '/img/logo.png' }],
+    ['link', { rel: 'apple-touch-icon', href: '/img/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'mask-icon', href: '/img/logo.png', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  plugins: [
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "Temos novidade",
+          buttonText: "Atualizar"
+        }
+      }
+    ]
+  ],
 }
